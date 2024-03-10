4e634753d5ff00e58e554d2ac9cf4dbeb1b58b29
Cal <calum.moore@gmail.com>
2024-03-09 22:37:11 -0500

    Update build.rs

--- build.rs
+++ build.rs
@@ -12,39 +12,7 @@ fn main() {
         println!("cargo:warning=Building for non-mobile platform");
     }
 
-    let lib_name = "zktrie";
-    let src_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let src_dir = Path::new(src_dir.as_str());
-
-    //#[cfg(not(target_os = "windows"))]
-    //let build_mode = gobuild::BuildMode::CArchive;
-    //#[cfg(target_os = "windows")]
-    let build_mode = gobuild::BuildMode::CShared;
-
-    // Build
-    if let Err(e) = gobuild::Build::new()
-        .buildmode(build_mode)
-        .try_compile(lib_name)
-    {
-        // The error type is private so have to check the error string
-        if format!("{e}").starts_with("Failed to find tool.") {
-            fail(
-                " Failed to find Go. Please install Go 1.16 or later \
-                following the instructions at https://golang.org/doc/install.
-                On linux it is also likely available as a package."
-                    .to_string(),
-            );
-        } else {
-            fail(format!("{e}"));
-        }
-    }
-
-    // file updating
-    let srcs = ["types", "trie", "lib.go", "c.go"];
-    for src in srcs {
-        let p = src_dir.join(src);
-        println!("cargo:rerun-if-changed={}", p.as_path().display());
-    }
+   return
 }
 
 fn fail(message: String) {
